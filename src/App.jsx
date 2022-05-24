9be9ecf8ac8ad59ac63b397e61856394dcb00348
DamianT325 <dtramowski789@interia.pl>
2022-05-24 17:44:49 +0200

    Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Slider from "./components/Slider";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <Home />
+      <Slider />
     </div>
   );
 };
