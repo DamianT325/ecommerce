3d3dc591e3f5cd7b7188d9fcb718a472cba7f639
DamianT325 <dtramowski789@interia.pl>
2022-05-29 14:41:14 +0200

    Product added

--- src/data.js
+++ src/data.js
@@ -47,7 +47,7 @@ export const popularProducts = [
   },
   {
     id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img: "http://cdn.shopify.com/s/files/1/0011/9783/4252/products/20_375a8763-f5d7-4184-a352-4523ef713733.jpg?v=1576267132",
   },
   {
     id: 3,
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://deichmann.scene7.com/asset/deichmann/US_01_400529_01?$rr_results$&defaultImage=default_obs",
   },
   {
     id: 8,
